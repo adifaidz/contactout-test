45b30b38a0fcd69d8270d2293d92db0a5cd1ea19
Adi Faidz Abdullah <adifaidzabdullah@gmail.com>
2022-07-14 21:44:54 +0800

    Fix link in email

--- resources/views/mail/referrals/invite.blade.php
+++ resources/views/mail/referrals/invite.blade.php
@@ -2,7 +2,8 @@
 {{ $name }} has been using ContactOut, and thinks it could be of use for you.
 
 Here's their invitation link for you:
-{{ $referralLink }}
+<a href="{{ $referralLink }}">{{ $referralLink }}</a><br>
+
 
 ContactOut gives you access to contact details for about 75% of the world's professionals.
 
@@ -11,7 +12,7 @@ Great for recruiting, sales, and marketing outreach.
 It's an extension that works right on top of LinkedIn.
 
 Here's their invitation link again:
-{{ $referralLink }}
+<a href="{{ $referralLink }}">{{ $referralLink }}</a><br>
 
 @component('mail::button', ['url' => $referralLink])
 Accept Invitation
